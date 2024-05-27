37046fe9987c68069bfed7d967b1d4e96f4fedec
Brenda Nahin Suarez Favreau <brendasu_97@hotmail.com>
2024-05-26 21:48:01 -0300

    calculadora

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/* Asi podemos hacer comentarios en JS */
+
+
+const NUEVE = 9; 
+
+for (let i = 1; i <= 10; i++) {
+
+    const RESULTADO = NUEVE * i; 
+
+    console.log (NUEVE + "x" + i + "=" + RESULTADO)
+}
